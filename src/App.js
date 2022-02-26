61134cb443f0b2884eecacfb6dadaad3eaa6053a
Christian Lara <christianramirezlara23@gmail.com>
2022-02-26 08:03:03 -0600

    Create the Game layout and add it the HeaderGame.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
+import Game from './layouts/Game/Game';
 import StartGame from './layouts/StartGame/StartGame';
 
 function App() {
   return (
     <div className="App">
-      <StartGame />
+      {/* <StartGame /> */}
+      <Game />
     </div>
   );
 }
---
+++ src/layouts/Game/Game.jsx
@@ -0,0 +1,12 @@
+import HeaderGame from '../../components/HeaderGame/HeaderGame';
+import './Game.scss';
+
+const Game = () => {
+  return (
+    <main className="game">
+      <HeaderGame />
+    </main>
+  );
+};
+
+export default Game;
