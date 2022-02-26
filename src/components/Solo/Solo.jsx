63aef3e20d444a4f9389e69ac386cacd93fc5601
Christian Lara <christianramirezlara23@gmail.com>
2022-02-26 14:59:52 -0600

    Implement the logic of generate random chips.

--- src/components/Solo/Solo.jsx
+++ src/components/Solo/Solo.jsx
@@ -3,25 +3,25 @@ import FooterItem from '../FooterItem/FooterItem';
 import './Solo.scss';
 
 const Solo = () => {
+  const numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+  const shuffle = (a) => {
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  };
+  const randomNumbersArr = shuffle(numbers);
+
   return (
     <>
       <section className="solo__grid">
-        <Chip />
-        <Chip />
-        <Chip />
-        <Chip />
-        <Chip />
-        <Chip />
-        <Chip />
-        <Chip />
-        <Chip />
-        <Chip />
-        <Chip />
-        <Chip />
-        <Chip />
-        <Chip />
-        <Chip />
-        <Chip />
+        {randomNumbersArr.map((number) => (
+          <Chip
+            key={'id' + Math.random().toString(16).slice(2)}
+            backTextContent={number}
+          />
+        ))}
       </section>
       <footer className="solo__footer">
         <FooterItem description="Time" value="0:00" />
