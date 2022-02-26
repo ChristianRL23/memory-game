6f425da0333bc4b39c9ea4e40f4c83220053d920
Christian Lara <christianramirezlara23@gmail.com>
2022-02-26 09:00:51 -0600

    Create and add a test grid.

---
+++ src/components/Solo/Solo.jsx
@@ -0,0 +1,30 @@
+import Chip from '../Chip/Chip';
+import './Solo.scss';
+
+const Solo = () => {
+  return (
+    <>
+      <section className="solo__grid">
+        <Chip />
+        <Chip />
+        <Chip />
+        <Chip />
+        <Chip />
+        <Chip />
+        <Chip />
+        <Chip />
+        <Chip />
+        <Chip />
+        <Chip />
+        <Chip />
+        <Chip />
+        <Chip />
+        <Chip />
+        <Chip />
+      </section>
+      <footer className="solo__footer"></footer>
+    </>
+  );
+};
+
+export default Solo;
--- src/layouts/Game/Game.jsx
+++ src/layouts/Game/Game.jsx
@@ -1,10 +1,12 @@
 import HeaderGame from '../../components/HeaderGame/HeaderGame';
+import Solo from '../../components/Solo/Solo';
 import './Game.scss';
 
 const Game = () => {
   return (
     <main className="game">
       <HeaderGame />
+      <Solo />
     </main>
   );
 };
