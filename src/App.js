55305f3dd511aea168bf48be83c6563eb070c495
Christian Lara <christianramirezlara23@gmail.com>
2022-02-27 17:34:08 -0600

    Create the GameOver component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import GameOver from './components/GameOver/GameOver';
+import SoloGameOver from './components/SoloGameOver/SoloGameOver';
 import Game from './layouts/Game/Game';
 import StartGame from './layouts/StartGame/StartGame';
 
@@ -8,7 +9,12 @@ function App() {
     <div className="App">
       {/* <StartGame /> */}
       <Game />
-      <GameOver />
+      <GameOver
+        message="You did it!"
+        description="Game over! Here's how you got on..."
+      >
+        <SoloGameOver />
+      </GameOver>
     </div>
   );
 }
--- src/components/GameOver/GameOver.jsx
+++ src/components/GameOver/GameOver.jsx
@@ -1,9 +1,19 @@
+import './GameOver.scss';
 import Modal from '../Modal/Modal';
+import Button from './../Button/Button';
 
-const GameOver = () => {
+const GameOver = ({ message, description, children }) => {
   return (
     <Modal>
-      <h1>Hello</h1>
+      <>
+        <h2 className="modal__message">{message}</h2>
+        <h6 className="modal__description">{description}</h6>
+      </>
+      {children}
+      <div className="modal__buttons">
+        <Button textContent="Restart" color="orange" />
+        <Button textContent="Setup New Game" color="gray" />
+      </div>
     </Modal>
   );
 };
