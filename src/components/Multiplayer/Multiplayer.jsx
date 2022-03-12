a62083b0f90e768f9eca3a0e0cebd5e73afc4b80
Christian Lara <christianramirezlara23@gmail.com>
2022-03-12 10:59:26 -0600

    Render the MultiplayerFooterItems necessaries for the number of players.

--- src/components/Multiplayer/Multiplayer.jsx
+++ src/components/Multiplayer/Multiplayer.jsx
@@ -1,14 +1,21 @@
+import { useSelector } from 'react-redux';
 import MultiplayerFooterItem from '../MultiplayerFooterItem/MultiplayerFooterItem';
 import './Multiplayer.scss';
 
 const Multiplayer = () => {
+  const gameConfigurationState = useSelector(
+    (state) => state.gameConfiguration
+  );
+
   return (
     <>
       <footer className="multiplayer__footer">
-        <MultiplayerFooterItem description="Player 1" value="0" />
-        <MultiplayerFooterItem description="Player 1" value="0" />
-        <MultiplayerFooterItem description="Player 1" value="0" />
-        <MultiplayerFooterItem description="Player 2" value="0" />
+        {[...Array(gameConfigurationState.players).keys()].map((number) => (
+          <MultiplayerFooterItem
+            description={`Player ${number + 1}`}
+            value="0"
+          />
+        ))}
       </footer>
     </>
   );
