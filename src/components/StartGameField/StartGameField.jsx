7d87791b7cb66c99bb1d3fe2b2790e69d1f2a64c
Christian Lara <christianramirezlara23@gmail.com>
2022-03-28 08:40:04 -0600

    Make responsive the StartGame layout.

--- src/components/StartGameField/StartGameField.jsx
+++ src/components/StartGameField/StartGameField.jsx
@@ -2,7 +2,7 @@ import './StartGameField.scss';
 
 const StartGameField = ({ children, label }) => {
   return (
-    <div>
+    <div className="start-game-field">
       <h6 className="start-game-field__label">{label}</h6>
       <div className="start-game-field__buttons">{children}</div>
     </div>
